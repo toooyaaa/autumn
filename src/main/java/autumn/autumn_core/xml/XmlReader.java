ed1e9e1841d36ffd88a3214e431d410b0292c023
toooyaaa <434538528@qq.com>
2015-08-14 01:02:06 +0800

    a

--- src/main/java/autumn/autumn_core/xml/XmlReader.java
+++ src/main/java/autumn/autumn_core/xml/XmlReader.java
@@ -1,10 +1,12 @@
 package autumn.autumn_core.xml;
 
+import org.dom4j.io.SAXReader;
+
 public class XmlReader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		SAXReader reader = new SAXReader();
+		SAXReader reader = new SAXReader();
 
 	}
 
