bb0c0e2057ba9bbdc4dda21b7f64febfdde275e3
toooyaaa <434538528@qq.com>
2015-08-15 22:56:47 +0800

    two

--- src/main/java/autumn/autumn_core/xml/XmlReader.java
+++ src/main/java/autumn/autumn_core/xml/XmlReader.java
@@ -1,5 +1,11 @@
 package autumn.autumn_core.xml;
 
+import java.io.File;
+import java.util.Iterator;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 public class XmlReader {
@@ -7,6 +13,23 @@ public class XmlReader {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SAXReader reader = new SAXReader();
+		try {
+			Document   document = reader.read(new File("src/main/resources/test.xml"));
+			Element root = document.getRootElement(); 
+			
+			for(Iterator it=root.elementIterator();it.hasNext();){    
+		        Element element = (Element) it.next();
+		        String id = element.attributeValue("id");
+		        String className = element.attributeValue("class");
+		        
+		        System.out.println(className);
+		        System.out.println(id);
+			}
+			
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}  
+		
 
 	}
 
